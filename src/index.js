737a38ea0feab6a1971022ce79720d8d61b51fef
Prachi I <prachi.indurkar007@gmail.com>
2023-03-25 20:39:44 +0530

    import store and provider in index.js file

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
 import App from './App';
+import Mystore from './store';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={Mystore}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 

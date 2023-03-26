eff6549f46c599d8257c727d2b8d571a442a4b0e
Prachi I <prachi.indurkar007@gmail.com>
2023-03-26 21:16:43 +0530

    import and call experiencelist in mainreducer

--- src/mainreducer.js
+++ src/mainreducer.js
@@ -1,10 +1,11 @@
 import { combineReducers } from "redux";
 import Basiclist from "./basicreducer";
+import Experiencelist from "./experiencereducer";
 // import Productlist from "./productreducer";
 // import Apilist from "./apireducer";
 
 const MainReducer = combineReducers({
-    Basiclist
+    Basiclist, Experiencelist
     //, Productlist, Apilist
 })
 export default MainReducer;
